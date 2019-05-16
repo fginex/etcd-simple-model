0b9c582d053fdeb5422cfe136ea3657085f2f7f0
Frank Ginex <fginex@peerstream.com>
2019-05-16 12:28:44 -0400

    fixed issue with peers unable to connect with each other. also setup to launch etcd with auto tls

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# ---
 
 docker container stop etcd1 etcd2 etcd3
 docker container rm etcd1 etcd2 etcd3
@@ -7,3 +8,14 @@ docker image rm nexcpu/etcd
 docker build -t nexcpu/etcd .
 
 docker-compose -f docker-compose.yml up
+
+# ---
+# To test once up and running: 
+# 1) Without TLS
+#          Execute the follwoing outside docker containers:
+#          ./etcdctl endpoint status
+#
+# 2) With Auto TLS
+#          Execute the follwoing inside one of the docker containers (ie etcd1):
+#          ./etcdctl --cert=etcd1.etcd/fixtures/client/cert.pem --key=etcd1.etcd/fixtures/client/key.pem --insecure-skip-tls-verify endpoint status
+# ---
